490915e81fead485497fd4051f96c8c89aa41ba2
mlsagar <sagar.maharjan617571@outlook.com>
2024-07-27 17:16:13 -0500

    Add: express, mongoose and dotenv package, .env file,and code to run static file in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT;
+const application = express();
+
+application.use(express.static(path.join(__dirname, "public")));
+
+
+application.listen(port, console.log(`Listening at ${port}`))
