4ec88e23c6810a10048280a6baf2556f01ba07dd
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-05 20:06:05 -0500

    Add: refactor the allcomments addcomments and getonecomments in comments controller

--- api/components/comments/comments.controller.js
+++ api/components/comments/comments.controller.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const mongoose = require("mongoose");
 const Article = mongoose.model(process.env.MODEL_NAME)
 const callbackify = require("util").callbackify;
@@ -6,31 +7,11 @@ const _Comment = function (artilceId) {
     return Article.findById(artilceId).select("comments");
 }
 
-
-const handleCommentSkipLimitExecCallbackify = function (articleId, offset, count) {
-    return _Comment(articleId).skip(offset).limit(count).exec();
-}
-const commentSkipLimitExecWithCallbackify = callbackify(handleCommentSkipLimitExecCallbackify);
-const handleAllComments = function (response, error, article) {
-    const responseCollection = {
-        status: 200,
-        message: article.comments
-    }
-    if (error) {
-        responseCollection.status = 500,
-        responseCollection.message = error
-    } else if(!article) {
-        responseCollection.status = 404,
-        responseCollection.message = {message: "Not found resource"}
-    }
-
-    response.status(responseCollection.status).json(responseCollection.message);
-}
 const allComments = function (request, response) {
     const articleId = request.params.articleId;
 
     if (!mongoose.isValidObjectId(articleId)) {
-        response.status(400).json({ message: "Invalid article id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_ARTICLE_ID_MESSAGE });
         return;
     }
     let offset = parseInt(process.env.INITIAL_FIND_OFFSET, process.env.RADIX_VALUE);
@@ -46,92 +27,67 @@ const allComments = function (request, response) {
     }
 
     if (isNaN(offset) || isNaN(count)) {
-        response.status(400).json({ message: "Invalid offset or count" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_OFFSET_COUNT_MESSAGE });
         return;
     }
 
     if (count > maxCount) {
-        response.status(400).json({ message: "Cannot exceed count of " + maxCount });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: `${process.env.MAX_LIMIT_MESSAGE} ${maxCount}` });
         return;
     }
 
-    commentSkipLimitExecWithCallbackify(articleId, offset, count, handleAllComments.bind(null, response));
-}
+    const responseCollection = _createResponseCollection();
 
+    Article.findById(articleId).select("comments").skip(offset).limit(count).exec()
+        .then(_handleAllComments.bind(null, responseCollection))
+        .catch(_setInternalError.bind(null, responseCollection))
+        .finally(_sendResponse.bind(null, response, responseCollection));
+}
 
 const handleArticleFindByIdSelectExecCallbackify = function (articleId) {
     return _Comment(articleId).exec();
 }
 const articleFindByIdSelectExecWithCallbackify = callbackify(handleArticleFindByIdSelectExecCallbackify);
-const handleAddArticle = function (request, response, error, article) {
-    const responseCollection = {
-        status: 200,
-        message: article
-    }
-    if (error) {
-        responseCollection.status = 500;
-        responseCollection.message = error;
-    } else if (!article) {
-        responseCollection.status = 404;
-        responseCollection.message = { message: "Article ID not found" };
-    }
 
-    if (article) {
-        _addComment(request, response, article);
-    } else {
-        response.status(responseCollection.status).json(responseCollection.message);
-    }
-}
 const addComment = function (request, response) {
     const articleId = request.params.articleId;
 
     if (!mongoose.isValidObjectId(articleId)) {
-        response.status(400).json({ message: "Invalid article id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_ARTICLE_ID_MESSAGE });
+        return;
     }
 
-    articleFindByIdSelectExecWithCallbackify(articleId, handleAddArticle.bind(null, request, response));
+    const responseCollection = _createResponseCollection();
 
+    Article.findById(articleId).select("comments").exec()
+        .then(_handleAddComment.bind(null, request, responseCollection))
+        .then(_setSuccessResponseCollectionAfterArticleSave.bind(null, responseCollection, process.env.COMMENT_POST_SUCCESS_MESSAGE))
+        .catch(_setInternalError.bind(null, responseCollection))
+        .finally(_sendResponse.bind(null, response, responseCollection));
 }
 
-
-const handleOneComment = function(response, commentId, error, article) {
-    const responseCollection = {
-        status: 200,
-        message: article
-    }
-    if (error) {
-        responseCollection.status = 500;
-        responseCollection.message = error;
-    } else if (!article) {
-        responseCollection.status = 404;
-        responseCollection.message = { message: "Article ID not found" };
-    }
-
-    if (article) {
-        if (!article.comments.id(commentId)) {
-            response.status(404).json({ message: "Comment ID not found" });
-            return;
-        }
-        response.status(200).json(article.comments.id(commentId));
-    } else {
-        response.status(responseCollection.status).json(responseCollection.message);
-    }
-}
 const oneComment = function (request, response) {
-    const articleId = request.params.articleId;
     const commentId = request.params.commentId;
+    const articleId = request.params.articleId;
+
     if (!mongoose.isValidObjectId(articleId)) {
-        response.status(400).json({ message: "Invalid article id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_ARTICLE_ID_MESSAGE });
+        return;
     }
     if (!mongoose.isValidObjectId(commentId)) {
-        response.status(400).json({ message: "Invalid comment id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_COMMENT_ID_MESSAGE });
     }
 
-    articleFindByIdSelectExecWithCallbackify(articleId, handleOneComment.bind(null, response, commentId));
+    const responseCollection = _createResponseCollection();
+
+    Article.findById(articleId).select("comments").exec()
+        .then(_handleOneComment.bind(null, responseCollection, commentId))
+        .catch(_setInternalError.bind(null, responseCollection))
+        .finally(_sendResponse.bind(null, response, responseCollection));
 }
 
 
-const handleUpdateComment = function(request, response, error, article) {
+const handleUpdateComment = function (request, response, error, article) {
     const responseCollection = {
         status: 200,
         message: article
@@ -160,24 +116,26 @@ const handleUpdateComment = function(request, response, error, article) {
     }
 }
 const fullUpdateOneComment = function (request, response) {
-    const articleId = request.params.articleId;
     const commentId = request.params.commentId;
+    const articleId = request.params.articleId;
+
     if (!mongoose.isValidObjectId(articleId)) {
-        response.status(400).json({ message: "Invalid article id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_ARTICLE_ID_MESSAGE });
+        return;
     }
     if (!mongoose.isValidObjectId(commentId)) {
-        response.status(400).json({ message: "Invalid comment id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_COMMENT_ID_MESSAGE });
     }
     articleFindByIdSelectExecWithCallbackify(articleId, handleUpdateComment.bind(null, request, response));
 }
 
 
 const partialUpdateOneComment = function (request, response) {
-    fullUpdateOneComment(request,response);
+    fullUpdateOneComment(request, response);
 }
 
 
-const handleDeleteComment = function(request, response, error, article) {
+const handleDeleteComment = function (request, response, error, article) {
     const responseCollection = {
         status: 200,
         message: article
@@ -205,15 +163,15 @@ const handleDeleteComment = function(request, response, error, article) {
     }
 }
 const comment = function (request, response) {
-    const articleId = request.params.articleId;
     const commentId = request.params.commentId;
+    const articleId = request.params.articleId;
     if (!mongoose.isValidObjectId(articleId)) {
-        response.status(400).json({ message: "Invalid article id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_ARTICLE_ID_MESSAGE });
+        return;
     }
     if (!mongoose.isValidObjectId(commentId)) {
-        response.status(400).json({ message: "Invalid comment id" });
+        response.status(Number(process.env.BAD_REQUEST_STATUS_CODE)).json({ message: process.env.INVALID_COMMENT_ID_MESSAGE });
     }
-
     articleFindByIdSelectExecWithCallbackify(articleId, handleDeleteComment.bind(null, request, response));
 }
 
@@ -235,16 +193,76 @@ const handleAddDeleteAndUpdateComment = function (action, response, error) {
 
     response.status(responseCollection.status).json(responseCollection.message);
 }
-const _addComment = function (request, response, article) {
+
+const _handleAllComments = function (responseCollection, article) {
+    if (!article) {
+        responseCollection.status = Number(process.env.BAD_REQUEST_STATUS_CODE);
+        responseCollection.message = { message: process.env.BAD_REQUEST_MESSAGE };
+        return;
+    }
+
+    responseCollection.status = Number(process.env.SUCCESS_STATUS_CODE);
+    responseCollection.message = article.comments;
+}
+
+const _handleAddComment = function (request, responseCollection, article) {
+    if (!article) {
+        responseCollection.status = Number(process.env.BAD_REQUEST_STATUS_CODE);
+        responseCollection.message = { message: process.env.BAD_REQUEST_MESSAGE };
+        return;
+    }
+
     const newComment = {
         comment: request.body.comment,
         name: request.body.name
     }
 
     article.comments.push(newComment);
+    return article.save();
 
-    articleSaveWithCallbackify(article, handleAddDeleteAndUpdateComment.bind(null, "Posted", response));
 }
+const _setSuccessResponseCollectionAfterArticleSave = function(responseCollection, message, saveComment) {
+    if (saveComment) {
+        responseCollection.status = Number(process.env.SUCCESS_STATUS_CODE);
+        responseCollection.message = { message };
+    }
+}
+
+const _handleOneComment = function (responseCollection, commentId, article) {
+    if (!article) {
+        responseCollection.status = Number(process.env.BAD_REQUEST_STATUS_CODE);
+        responseCollection.message = { message: process.env.BAD_REQUEST_MESSAGE };
+        return;
+    }
+
+    
+    if (!article.comments.id(commentId)) {
+        responseCollection.status = Number(process.env.BAD_REQUEST_STATUS_CODE);
+        responseCollection.message = {message: process.env.COMMENT_ID_NOT_FOUND_MESSAGE}
+        return;
+    }
+
+    responseCollection.status = Number(process.env.SUCCESS_STATUS_CODE);
+    responseCollection.message = article.comments.id(commentId);
+    
+}
+
+const _createResponseCollection = function () {
+    return {
+        status: Number(process.env.CREATE_STATUS_CODE),
+        message: ""
+    }
+}
+
+const _setInternalError = function (responseCollection, error) {
+    responseCollection.status = Number(process.env.SERVER_ERROR_STATUS_CODE);
+    responseCollection.message = error;
+}
+
+const _sendResponse = function (response, responseCollection) {
+    response.status(responseCollection.status).json(responseCollection.message)
+}
+
 
 module.exports = {
     allComments,
