10219bc7024331ab761714c38bf822c0d408f9ce
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-08 17:04:19 -0500

    Add: if case for error message of users controller

--- api/components/users/users-controller.js
+++ api/components/users/users-controller.js
@@ -106,7 +106,7 @@ const _createResponseCollection = function () {
 
 const _setInternalError = function (responseCollection, error) {
     responseCollection.status = responseCollection.status === Number(process.env.CREATE_STATUS_CODE) ? Number(process.env.SERVER_ERROR_STATUS_CODE) : responseCollection.status;
-    responseCollection.message = error;
+    responseCollection.message = error.message || error;
 }
 
 const _sendResponse = function (response, responseCollection) {
