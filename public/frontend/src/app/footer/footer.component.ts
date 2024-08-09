e19d8c471314d5405b330917cdbe6729e653deb5
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-09 16:10:22 -0500

    Add: footer design in the frontend

--- public/frontend/src/app/footer/footer.component.ts
+++ public/frontend/src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  currentYear!: number;
 
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear()
+  }
 }
