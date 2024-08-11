6d88e22cc8e1e461acef3b3257714be93b06ba1e
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-11 07:58:14 -0500

    Add: component for dropdown menu

---
+++ public/frontend/src/app/shared/dropdown-menu/dropdown-menu.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ClickOutsideDirective } from '../../click-outside.directive';
+import { RouterLink } from '@angular/router';
+
+export interface Dropdown {
+  name: string;
+  routeLink: string;
+}
+
+export interface DropdownConfig {
+  config: Dropdown[]
+}
+
+@Component({
+  selector: 'app-dropdown-menu',
+  standalone: true,
+  imports: [CommonModule, ClickOutsideDirective, RouterLink],
+  templateUrl: './dropdown-menu.component.html',
+  styleUrl: './dropdown-menu.component.css'
+})
+export class DropdownMenuComponent {
+  showMenu = false;
+
+  @Input()
+  dropdownConfig!: DropdownConfig;
+
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  clickOutside() {
+    if(this.showMenu) {
+      this.showMenu = false;      
+    }
+  }
+}
