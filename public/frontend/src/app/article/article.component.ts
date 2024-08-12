d1ca76dd17e45cbcc080b7ccd383b30c592767c9
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-12 08:36:54 -0500

    Add: make dynamic of article component

--- public/frontend/src/app/article/article.component.ts
+++ public/frontend/src/app/article/article.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DisplayCommentComponent } from '../display-comment/display-comment.component';
 import { AddCommentComponent } from '../add-comment/add-comment.component';
+import { Article } from '../articles-data.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [CommonModule, DisplayCommentComponent, AddCommentComponent],
+  imports: [CommonModule, DisplayCommentComponent, AddCommentComponent, RouterLink],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
-
+  @Input() article!: Article;
+  @Input() showCommentsSection = false
 }
