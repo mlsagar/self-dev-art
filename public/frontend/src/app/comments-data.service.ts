078e987dba39ec681c7b3df92075be468e82d900
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-16 07:13:19 -0500

    Refactor: comments data service

--- public/frontend/src/app/comments-data.service.ts
+++ public/frontend/src/app/comments-data.service.ts
@@ -20,29 +20,37 @@ export interface CommentWithArticleId extends Comment {
   providedIn: 'root'
 })
 export class CommentsDataService {
-  baseUrl = environment.BASE_URL;
   routes = environment.ROUTES;
+  baseUrl = environment.BASE_URL;
   constructor(
     private _http: HttpClient
   ) { }
 
   allComments(postId: string) {
-    return this._http.get<Response<Comment>>(this.baseUrl + "/" + postId + this.routes.COMMENTS);
+    return this._http.get<Response<Comment>>(this._getCommentsUrl(postId));
   }
 
   addComment(postId: string, commentRequest: CommentRequest) {
-    return this._http.post<Response<any>>(this.baseUrl + this.routes.ARTICLES + "/" + postId + this.routes.COMMENTS, commentRequest);
+    return this._http.post<Response<any>>(this._getCommentsUrl(postId), commentRequest);
   }
 
   fullUpdate(postId: string, commentId: string, commentRequest: CommentRequest) {
-    return this._http.put<Response<any>>(this.baseUrl + this.routes.ARTICLES + "/" + postId + this.routes.COMMENTS + "/" + commentId, commentRequest);
+    return this._http.put<Response<any>>(this._getCommentsUrlWithId(postId, commentId), commentRequest);
   }
 
   partialUpdate(postId: string, commentId: string, commentRequest: Partial<CommentRequest>) {
-    return this._http.patch<Response<any>>(this.baseUrl + this.routes.ARTICLES + "/" + postId + this.routes.COMMENTS + "/" + commentId, commentRequest);
+    return this._http.patch<Response<any>>(this._getCommentsUrlWithId(postId, commentId), commentRequest);
   }
 
   deleteComment(postId: string, commentId: string) {
-    return this._http.delete<Response<any>>(this.baseUrl + this.routes.ARTICLES + "/" + postId + this.routes.COMMENTS + "/" + commentId);
+    return this._http.delete<Response<any>>(this._getCommentsUrlWithId(postId, commentId));
+  }
+
+  _getCommentsUrl(postId: string) {
+    return this.baseUrl + this.routes.ARTICLES + "/" + postId + this.routes.COMMENTS;
+  }
+
+  _getCommentsUrlWithId(postId: string, commentId: string) {
+    return this._getCommentsUrl(postId) + "/" + commentId;
   }
 }
