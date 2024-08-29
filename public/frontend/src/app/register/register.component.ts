d38b377b5b91c29f43c12e18d6e0037137876f03
mlsagar <sagar.maharjan617571@outlook.com>
2024-08-29 08:34:40 -0500

    Refactor: the code of register component

--- public/frontend/src/app/register/register.component.ts
+++ public/frontend/src/app/register/register.component.ts
@@ -4,7 +4,7 @@ import { FormsModule, NgForm } from "@angular/forms";
 import { Router } from '@angular/router';
 import { finalize } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { ImageUploadService } from '../../services/image-upload.service';
+import { ImageUploadService, ImageUrl } from '../../services/image-upload.service';
 import { AuthService } from '../auth.service';
 import { ComparePasswordDirective } from '../compare-password.directive';
 import { ErrorResponse, Response } from '../reponse';
@@ -18,26 +18,26 @@ import { User, UsersDataService } from '../users-data.service';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   isButtonDisabled = false;
   routes = environment.ROUTES;
 
   @ViewChild("registerForm")
-    registerForm!: NgForm;
+  registerForm!: NgForm;
 
   constructor(
     private _usersDataService: UsersDataService,
     private _authService: AuthService,
     private _router: Router,
     private _toast: ToastService,
-	private _imageUploadService: ImageUploadService
-  ) {}
+    private _imageUploadService: ImageUploadService
+  ) { }
 
   ngOnInit(): void {
-    if(this._authService.isLoggedIn) {
+    if (this._authService.isLoggedIn) {
       this._router.navigate([this.routes.HOME]);
     }
-    setTimeout(() => {this._resetRegistrationForm()}, 0)
+    setTimeout(() => { this._resetRegistrationForm() }, 0)
   }
 
   register() {
@@ -46,52 +46,51 @@ export class RegisterComponent implements OnInit{
       return;
     }
     this.isButtonDisabled = true;
-    const {confirmPassword, fileImage, ...userData} = this.registerForm.value;
+    const { confirmPassword, fileImage, ...userData } = this.registerForm.value;
     this._callRegisterApi(userData);
-   }
-
-   uploadImage(event: Event) {
-    const file = (event.target as HTMLInputElement).files;
-	if (file) {
-		const formData = new FormData();
-		formData.append("image", file[0])
-		this._imageUploadService.uploadSingleImage(formData).subscribe({
-			next: (response) => {
-				this.registerForm.form.patchValue({
-          image: response.data[0].url
-        });
-        console.log(this.registerForm.value);
-			},
-			error: (error) => {
-				console.log(error);
-			}
-		})
-	}
-   }
- 
-   _callRegisterApi(userData: User) {
-    this._usersDataService.register(userData)
-    .pipe(finalize(this._enablingButton.bind(this)))
-    .subscribe({
-      next: this._handleRegiterApiSuccess.bind(this),
-      error: this._handleRegiterApiError.bind(this)
-    })
-   }
-
-   _handleRegiterApiSuccess(response: Response<any>) {
-    this._resetRegistrationForm();
-    this._toast.open({type: MESSAGE_TYPE.SUCCESS, message: response.message});
-   }
-
-   _handleRegiterApiError(err: ErrorResponse<any>) {
-    this._toast.open({type: MESSAGE_TYPE.ERROR, message: err.error.message})
   }
 
-   _resetRegistrationForm() {    
-    this.registerForm.reset();
-   }
+  uploadImage(event: Event) {
+    const file = (event.target as HTMLInputElement).files;
+    if (file) {
+      const formData = new FormData();
+      formData.append("image", file[0]);
+      this._imageUploadService.uploadSingleImage(formData).subscribe({
+        next: this._handleUploadSingleImageSuccess.bind(this),
+        error: this._handleApiError.bind(this)
+      })
+    }
+  }
 
-   _enablingButton() {
+  _handleUploadSingleImageSuccess(response: Response<ImageUrl>) {
+    this.registerForm.form.patchValue({
+      image: response.data[0].url
+    });
+  }
+
+  _callRegisterApi(userData: User) {
+    this._usersDataService.register(userData)
+      .pipe(finalize(this._enablingButton.bind(this)))
+      .subscribe({
+        next: this._handleRegiterApiSuccess.bind(this),
+        error: this._handleApiError.bind(this)
+      })
+  }
+
+  _handleRegiterApiSuccess(response: Response<any>) {
+    this._resetRegistrationForm();
+    this._toast.open({ type: MESSAGE_TYPE.SUCCESS, message: response.message });
+  }
+
+  _handleApiError(err: ErrorResponse<any>) {
+    this._toast.open({ type: MESSAGE_TYPE.ERROR, message: err.error.message })
+  }
+
+  _resetRegistrationForm() {
+    this.registerForm.reset();
+  }
+
+  _enablingButton() {
     this.isButtonDisabled = false
   }
 
